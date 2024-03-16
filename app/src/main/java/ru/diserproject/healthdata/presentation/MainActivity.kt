56de6efb8430153a07c4dcaa45370e43ebb2eab5
Shepard-PC <jeltov.dima@yandex.ru>
2024-03-17 01:15:48 +0300

    App now Requests permissions

--- app/src/main/java/ru/diserproject/healthdata/presentation/MainActivity.kt
+++ app/src/main/java/ru/diserproject/healthdata/presentation/MainActivity.kt
@@ -1,27 +1,46 @@
 package ru.diserproject.healthdata.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ru.diserproject.healthdata.R
 
 class MainActivity : ComponentActivity(),SensorEventListener {
     private lateinit var sensorManager: SensorManager
+
+    private  val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        isGranted: Boolean ->
+        if (isGranted)
+            Log.i("Permission","Granted")
+        else
+            Log.i("Permission","Denied")
+    }
+
+    private var _permissionList = listOf<String>(
+        Manifest.permission.BODY_SENSORS,
+        Manifest.permission.BODY_SENSORS_BACKGROUND,
+        Manifest.permission.WAKE_LOCK,
+    )
+    private var _problematicPermissions = mutableListOf<String>()
     //UI
     private lateinit var heartTextView: TextView
     private  lateinit var presureTextView: TextView
     //Sensors
     private var heartSensor: Sensor? = null
-    private var presureSensor: Sensor? = null
+    private var pressureSensor: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
-
         super.onCreate(savedInstanceState)
 
         setTheme(android.R.style.Theme_DeviceDefault)
@@ -30,13 +49,34 @@ class MainActivity : ComponentActivity(),SensorEventListener {
         heartTextView = findViewById(R.id.heartRateTextView)
         presureTextView = findViewById(R.id.pressureTextView)
 
+        _permissionList.forEach(){
+            if (ContextCompat.checkSelfPermission(this,it) != PackageManager.PERMISSION_GRANTED)
+                _problematicPermissions.add(it)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        requestPermission()
         setUpSensor()
     }
 
+    private fun requestPermission(){
+        when{
+            !_problematicPermissions.any() -> {
+                // Разрешения получены
+            }
+            else -> {
+                _problematicPermissions.forEach(){
+                    requestPermissionLauncher.launch(it)
+                }
+            }
+        }
+    }
     private fun setUpSensor(){
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         heartSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE)
-        presureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
+        pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -55,7 +95,7 @@ class MainActivity : ComponentActivity(),SensorEventListener {
     override fun onResume() {
         super.onResume()
         sensorManager.registerListener(this,heartSensor,SensorManager.SENSOR_DELAY_NORMAL,SensorManager.SENSOR_DELAY_UI)
-        sensorManager.registerListener(this,presureSensor,SensorManager.SENSOR_DELAY_NORMAL,SensorManager.SENSOR_DELAY_UI)
+        sensorManager.registerListener(this,pressureSensor,SensorManager.SENSOR_DELAY_NORMAL,SensorManager.SENSOR_DELAY_UI)
     }
 
     override fun onDestroy() {
